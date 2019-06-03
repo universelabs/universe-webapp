4d1aa4335be88b5954eba5ba0453a1942fe1c553
Modestos Kaffes <modkaffes@gmail.com>
2019-06-03 03:16:49 +0300

    Create Accordion component

---
+++ client/src/components/Accordion.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const styles = {
+  list: {
+    margin: 0
+  },
+  item: {
+    cursor: 'pointer'
+  },
+  itemTitle: {
+    userSelect: 'none',
+    margin: '-0.75rem -0.125rem',
+    padding: '0.75rem 0.125rem'
+  },
+  itemContent: {
+    margin: '0.75rem -1.25rem -0.75rem',
+    padding: '0 1.25rem',
+    borderTop: '1px solid #eee',
+    backgroundColor: 'white'
+  }
+};
+
+export const AccordionItem = ({ title, children }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <li className="list-group-item list-group-item-action" style={styles.item}>
+      <div style={styles.itemTitle} onClick={() => setExpanded(!expanded)}>
+        {title}
+      </div>
+      {expanded && <div style={styles.itemContent}>{children}</div>}
+    </li>
+  );
+};
+
+const Accordion = ({ children }) => (
+  <ul className="list-group" style={styles.list}>
+    {children}
+  </ul>
+);
+
+export default Accordion;
