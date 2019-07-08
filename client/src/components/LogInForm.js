37428aad3a5b3de7f98ced00fc89c20a051f6efb
Modestos Kaffes <modkaffes@gmail.com>
2019-07-08 12:11:37 +0300

    Add functionality to login form

--- client/src/components/LogInForm.js
+++ client/src/components/LogInForm.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'redux-react-hook';
+import * as actions from '../constants/actions';
+import useForm from '../hooks/useForm';
 
-function LogInForm() {
+function LogInForm({ history }) {
+  const [loading, setLoading] = useState();
+  const dispatch = useDispatch();
+  const { values, handleChange, handleSubmit } = useForm(
+    { email: '', password: ''},
+    postLogIn
+  );
+
+  async function postLogIn() {
+    try {
+      setLoading(true);
+
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_EXPRESS_SERVER}/login`,
+        values
+      );
+      
+      const res = await data;
+
+      if (res.error) {
+        console.error(res.error);
+        return history.push('/signup-error');  
+      }
+
+      if (res.token) {
+        dispatch({
+          type: actions.SET_AUTH_USER,
+          authUser: {
+            email: values.email
+          }
+        });
+  
+        localStorage.setItem('token', res.token);
+  
+        setLoading(false);
+        return history.push('/');
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
+
   return (
-    <form action="/login" method="POST">
+    <form onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="email" className="d-none">
           Email address
         </label>
         <input
+          onChange={handleChange}
+          value={values.email}
           name="email"
           type="email"
           id="email"
           className="form-control mb-2"
           placeholder="Email address"
-          required={true}
+          required
           autoComplete="off"
           tabIndex="2"
           autoCapitalize="none"
@@ -26,12 +75,14 @@ function LogInForm() {
           Password
         </label>
         <input
+          onChange={handleChange}
+          value={values.password}
           name="password"
           type="password"
           id="password"
           className="form-control mb-2"
           placeholder="Password"
-          required={true}
+          required
           autoComplete="off"
           tabIndex="3"
           aria-invalid="false"
@@ -43,13 +94,14 @@ function LogInForm() {
       <div>
         <input
           type="submit"
-          value="Log In"
+          value={loading ? 'Logging in…' : 'Log In'}
+          disabled={loading}
           tabIndex="4"
-          className="btn btn-lg btn-black btn-subscribe btn-block"
+          className="btn btn-black btn-subscribe btn-block"
         />
       </div>
     </form>
   );
 }
 
-export default LogInForm;
+export default withRouter(LogInForm);
