5a271e9e47b3ae96282d57aec7cc66588294df90
Modestos Kaffes <modkaffes@gmail.com>
2019-08-02 20:36:22 +0300

    Add hook to handle feather bluetooth connection

--- client/src/containers/Feather.js
+++ client/src/containers/Feather.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import CopyrightUpdate from 'copyright-update';
 import logoWordmark from './../img/universe-wordmark.svg';
+import useBTConnected from '../hooks/useBTConnected';
 
 function Feather() {
+  const isConnected = useBTConnected();
+
   return (
     <div className="container-fluid text-center">
       <div className="mt-5 mb-5">
@@ -16,9 +19,20 @@ function Feather() {
         <div className="mt-3">Welcome to Universe</div>
       </div>
       <div className="col-md-8 offset-md-2">
-        <strong className="lead font-weight-bold">
-          Your Universe Feather device is not connected.
-        </strong>
+        {!isConnected && (
+          <strong className="lead font-weight-bold">
+            Your Universe Feather device is not connected.
+          </strong>
+        )}
+        {isConnected && (
+          <strong className="lead font-weight-bold">
+            Your Universe Feather device is now connected. Please{' '}
+            <a href="/signup" className="link-dynamic-dark">
+              sign up
+            </a>{' '}
+            for Universe.
+          </strong>
+        )}
         <p className="mt-4">
           Please follow the setup instructions that came with your device <br />
           or{' '}
